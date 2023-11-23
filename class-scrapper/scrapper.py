644e114ec9ccc069364c0d7fd3cbf2ab715156ab
Emmanuel Cobian Duarte <ecobian12310@gmail.com>
2023-11-22 22:57:03 -0800

    added class prereq info

--- class-scrapper/scrapper.py
+++ class-scrapper/scrapper.py
@@ -5,7 +5,7 @@ from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-departments = ['math', 'compsci', 'eecs', 'data', 'physics']
+departments = ['math', 'data', 'eecs', 'compsci', 'physics']
 for website_path in departments:
     options = Options()
     options.add_argument('--headless=new')
@@ -20,19 +20,37 @@ for website_path in departments:
 
 
     resultObj = dict()
+    file_path = 'class-scrapper/' + website_path + "_scraping_results.json"
+    data = None
+    with open(file_path, 'r') as file:
+        data = json.load(file)
     driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/main/div/div/div/div[1]/button[1]').click()
     '/html/body/div[2]/div[2]/main/div/div[2]/div/div[1]/button[1]'
     time.sleep(1)
 
     classes = driver.find_elements(By.CLASS_NAME, 'courseblock')
     for i, cls in enumerate(classes):
-        classCode = cls.find_element(By.XPATH, './/button/h3/span[1]')
-        classTitle = cls.find_element(By.XPATH, './/button/h3/span[2]')
-        classUnits = cls.find_element(By.XPATH, './/button/h3/span[3]')
-        classDesc = cls.find_element(By.XPATH, './/div/p')
-        resultObj[classCode.text] = {"Title": classTitle.text, "Units": classUnits.text, "Description": classDesc.text.split('\n')[-1]}
+        classCode = cls.find_element(By.XPATH, './/button/h3/span[1]').text
+        classTitle = cls.find_element(By.XPATH, './/button/h3/span[2]').text
+        classUnits = cls.find_element(By.XPATH, './/button/h3/span[3]').text
+        classDesc = cls.find_element(By.XPATH, './/div/p').text.split('\n')[-1]
+        classPreReq = cls.find_element(By.XPATH, './/div/div/div[1]/p[2]').text
+        prefix = 'Prerequisites: '
+        if not classPreReq.startswith(prefix):
+            try:
+                classPreReq = cls.find_element(By.XPATH, './/div/div/div[2]/p[2]').text
+                classPreReq = 'N/A' if not classPreReq.startswith(prefix) else classPreReq[len(prefix):]
+            except:
+                classPreReq = "N/A"
+        else:
+            classPreReq = classPreReq[len(prefix):]
+
+        # resultObj[classCode] = {"Title": classTitle, "Units": classUnits, "Description": classDesc, 'Prerequisites': classPreReq}
+        if classCode in data:
+            data[classCode]['Prerequisites'] = classPreReq
+        else:
+            print('key not found', classCode)
 
     driver.quit()
-    file_path = 'class-scrapper/' + website_path + "_scraping_results.json"
     with open(file_path, 'w') as file:
-        json.dump(resultObj, file)
+        json.dump(data, file)
