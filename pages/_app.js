0f5337a9030ff212eaa3949d8d29e6a0246fa76f
jackiedai <jackie123768@gmail.com>
2023-11-29 16:04:11 -0800

    Add authentication system

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import '../styles/Login.module.css'
+import { SessionProvider } from "next-auth/react"
+import { AppProps } from "next/app"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
