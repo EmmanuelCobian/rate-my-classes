2a131e10ead9a8edebad98b5f68afc61d334e840
Jeffrey Millan <jmillan736@berkeley.edu>
2023-12-01 13:26:31 -0800

    doneish with student reviews

--- components/StudentReview.js
+++ components/StudentReview.js
@@ -1,41 +1,42 @@
-import Button from "react-bootstrap/Button";
+
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import styles from "@/styles/StudentReview.module.css";
-import { Condiment } from "next/font/google";
+
 
 const StudentReview = (props) => {
   return (
-    <Card style={{ width: "40rem", backgroundColor: "rgb(217,217,217)" }}>
-      <Container>
-        <Row>
-          <Col sm md="3" className="text-center">
-            <Card.Body className="pb-0 mt-4">
-              <Card.Subtitle className="mb-1"><strong>DIFFICULTY</strong></Card.Subtitle>
-              <div className="d-flex justify-content-center align-items-center">
-                <Card bg="dark" text="white" style={{ width: "4rem", textAlign: "center" }}>
+    <Card style={{ width: "600px",minWidth:"570px", height:"260px", backgroundColor: "rgb(217,217,217)"}}>
+        <Row className="h-100 w-100">
+          <Col xs={3} className="p-0 text-center position-relative d-flex flex-column">
+            <Card.Body className="p-0 position-absolute start-50 top-50 translate-middle">
+              <div className="pb-2">
+                <Card.Subtitle className="mb-1"><strong>DIFFICULTY</strong></Card.Subtitle>
+                <div className="d-flex justify-content-center align-items-center">
+                  <Card bg="dark" text="white" style={{ width: "4rem", textAlign: "center" }}>
+                      <Card.Body>
+                        <Card.Title>{props.diffRating}</Card.Title>
+                      </Card.Body>
+                  </Card>
+                </div>
+              </div>
+              <div>
+                <Card.Subtitle className="mb-1"><strong>INTEREST</strong></Card.Subtitle>
+                <div className="d-flex justify-content-center align-items-center">
+                  <Card bg="dark" text="white" style={{ width: "4rem", textAlign: "center" }}>
                     <Card.Body>
-                      <Card.Title>{props.diffRating}</Card.Title>
+                      <Card.Title>{props.intRating}</Card.Title>
                     </Card.Body>
-                </Card>
+                  </Card>
+                </div>
               </div>
-            </Card.Body>
-            <Card.Body className="mb-4">
-              <Card.Subtitle className="mb-1"><strong>INTEREST</strong></Card.Subtitle>
-              <div className="d-flex justify-content-center align-items-center">
-                <Card bg="dark" text="white" style={{ width: "4rem", textAlign: "center" }}>
-                  <Card.Body>
-                    <Card.Title>{props.intRating}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </div>
+
             </Card.Body>
           </Col>
-          <Col sm md="9" style={{padding: "0"}}>
-            <Card.Body style={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", paddingTop:"28px", paddingBottom:"28px", paddingLeft:"0"}}>
+          <Col xs={9} style={{padding: "0"}}>
+            <Card.Body style={{ height: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", paddingTop:"16px", paddingBottom:"8px", paddingLeft:"0"}}>
               <Container style={{padding: "0"}}>
                 <Card.Text className="mb-0">
                   Professor: <strong>{props.profe}</strong> Term: <strong>{props.term}</strong>
@@ -51,7 +52,6 @@ const StudentReview = (props) => {
             </Card.Body>
           </Col>
         </Row>
-      </Container>
     </Card>
   );
 };
