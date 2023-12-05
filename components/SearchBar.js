45bf8ccf1777fceb1cb903ca26a7192889638f42
Emmanuel Cobian Duarte <ecobian12310@gmail.com>
2023-12-04 16:23:31 -0800

    added invalid class search error handling from the search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import React from "react";
+import React, { isValidElement } from "react";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const SearchInput = styled.input`
   padding: 15px;
@@ -15,44 +16,63 @@ const SearchInput = styled.input`
   backdrop-filter: blur(8px);
   margin: 0 auto;
   margin-top: auto;
-  margin-bottom: 180px;
+  margin-bottom: ${({ isValidInput }) => (isValidInput ? "180px" : "0px")};
+
+  &:focus {
+    border: 1px solid ${({ isValidInput }) => (isValidInput ? "blue" : "red")};
+  }
+`;
+
+const SearchError = styled.p`
+  color: red;
+  margin-bottom: ${({ isValidInput }) => (isValidInput ? "0px" : "180px")};
+  display: ${({ isValidInput }) => (isValidInput ? "none" : "block")};
 `;
 
 export default function SearchBar() {
+  const [validInput, setValidInput] = useState(true);
   const router = useRouter();
 
   const testIfExists = async (params) => {
-    const department = params[0]
-    const courseCode = params.join(" ")
-    return fetch(`http://localhost:2000/${department}?code=${courseCode}`)
-      .then((response) => {
+    const department = params[0];
+    const courseCode = params.join(" ");
+    return fetch(`http://localhost:2000/${department}?code=${courseCode}`).then(
+      (response) => {
         if (!response.ok) {
-          return false
+          return false;
         } else {
-          return true
+          return true;
         }
-      })
-  }
+      }
+    );
+  };
 
   // Handles inputs from search bar.
   const handleOnKeyDown = async (e) => {
     if (e.key != "Enter") {
-      return
+      return;
     }
-    let search = e.target.value.split(" ")
+    let search = e.target.value.split(" ");
     if (await testIfExists(search)) {
+      setValidInput(true);
       router.push(`/classes/${search.join("/")}`);
     } else {
-      console.log("that class doesn't exist")
+      setValidInput(false);
     }
   };
 
   return (
-    <SearchInput
-      type="text"
-      className="search-input"
-      placeholder="Search by name, professor, class id..."
-      onKeyDown={(e) => handleOnKeyDown(e)}
-    />
+    <div>
+      <SearchInput
+        type="text"
+        className="search-input"
+        isValidInput={validInput}
+        placeholder="Search by name"
+        onKeyDown={(e) => handleOnKeyDown(e)}
+      />
+      <SearchError isValidInput={validInput}>
+        That course doesn't exist, please try again.
+      </SearchError>
+    </div>
   );
 }
