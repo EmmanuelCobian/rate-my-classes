b3d757d0b5d8f343ddaedafa6a0de4f0b0aa2c8e
Emmanuel Cobian Duarte <ecobian12310@gmail.com>
2023-12-06 17:53:42 -0800

    project ready to make public

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github"
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: "76159749ce1a1f0f817e",
-      clientSecret: "62ae68ac62e5b68c4a851c10b0b9a7b9a72cba26",
+      clientId: process.env.clientId,
+      clientSecret: process.env.clientSecret,
     }),
   ],
 }
